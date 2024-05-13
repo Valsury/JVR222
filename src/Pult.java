0f56d2eac2d6ecb9007bcd10b35a1c23857fb713
valsu <valsury@mail.ru>
2024-05-13 14:14:51 +0300

    Start

---
+++ src/Pult.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+
+public class Pult extends JFrame {
+
+    public static void myCreateAndShowAPI() {
+
+        Pult frame = new Pult();
+
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+
+        frame.setPreferredSize(new Dimension(1580, 850));
+
+        frame.pack();
+
+        frame.setVisible(true);
+
+    }
+
+}
